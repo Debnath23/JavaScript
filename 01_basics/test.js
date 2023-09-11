ba0c12813f530c8a3f9ea420c89629f1737820be
Debnath23 <128223356+Debnath23@users.noreply.github.com>
2023-09-11 06:57:26 +0000

    Final commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
-console.log("Debnath_Mahapatra");
+const accountId = 12345;
+let email = "deb@gmail.com";
+var password = "123";
+city = "Kolkata";
+let state;
+
+// accountId = 23456 is not allowed because of const keyword
+
+email = "mahapatra@gmail.com";
+password = "232323";
+city = "Bengaluru";
+
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.log([accountId, email, password, city, state]);
+console.table([accountId, email, password, city, state]);
