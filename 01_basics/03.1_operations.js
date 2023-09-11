70c4e70503dbe60b3b2e7eb0d32924c71fe4e298
Debnath23 <128223356+Debnath23@users.noreply.github.com>
2023-09-11 15:18:44 +0000

    Operations

---
+++ 01_basics/03.1_operations.js
@@ -0,0 +1,21 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log("1" + 2);
+console.log("1" + "2");
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+// Not to be followed
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+let n1, n2, n3;
+n1 = n2 = n3 = 2 + 2
