79eeb6e017ff31a94e64add71c7b5dc585161d4b
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-04-05 07:29:12 +0000

    Scope concept is modified

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -11,6 +11,13 @@ if(true){
 // console.log(b);
 console.log(c);
 
+/* let and const have block-level scope, meaning they are only accessible within the block they are defined in.
+var has function-level scope, meaning it's accessible throughout the entire function in which it is defined or globally if defined outside any function.
+
+a is declared using let inside the if block. Therefore, it's not accessible outside the block. So, console.log(a) will throw a ReferenceError.
+b is declared using const inside the if block. Similar to let, it's not accessible outside the block. So, console.log(b) will also throw a ReferenceError.
+c is declared using var, which has function-level scope. So, when you declare c inside the if block, it actually modifies the outer c variable. Therefore, console.log(c) will print 30. */
+
 function one(){
     const username = "Debnath"
 
